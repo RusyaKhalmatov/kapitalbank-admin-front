4a7d68f0187e5d263c8c413cd6d5c5d372b8c332
Elmira <timra.work@gmail.com>
2021-10-27 21:53:26 +0500

    feat: 🎸 use mock data in development

--- src/components/report/ReportServiceProvidersTotal.vue
+++ src/components/report/ReportServiceProvidersTotal.vue
@@ -2,6 +2,7 @@
   <div>
     <h1 class="text-xs-center mb-3 mt-3">Общее колличество проверок провайдеров</h1>
     <date-component @date="getDate"></date-component>
+    {{operationAmount}}
     <div class="button-box">
         <v-btn dark color="primary" class="get-btn" @click="loadAmount" :loading="loader">Получить</v-btn>
         <!-- <v-btn class="mt-3 excel-btn" :loading="loader" @click="getExcel">Скачать Excel</v-btn>
@@ -27,6 +28,7 @@ import ReportDateTimePicker from './ReportDateTimePicker.vue';
 import { DATE_FORMAT, MONTH_AGO_DATE } from './ReportAllBanks.vue';
 import DateComponent from '../date/DateComponent.vue';
 import ReportTotal from '../../views/ReportTotal.vue';
+import json from './reportDemoData/reportServiceProvider.json';
 
 export default {
   components: { ReportDateTimePicker, DateComponent, ReportTotal },
@@ -34,6 +36,7 @@ export default {
   data() {
     return {
       apiUrl: '',
+      operationAmount: null,
       operation: {
         dateEnd: moment().format(DATE_FORMAT),
         dateStart: moment(MONTH_AGO_DATE).format(DATE_FORMAT),
@@ -49,18 +52,23 @@ export default {
   },
   methods: {
     showEnvVariable() {
-      console.log('env = ', process.env.TZ)
+      console.log('env = ', process.env.NODE_ENV);
+      console.log('process.env = ', process.env);
     },
     showErrorInfo(item) {
       this.errorInfo = item;
       this.errorDialog = true;
     },
     loadAmount() {
-      this.$http.get(this.apiUrl)
-        .then(response => {
-          this.operationAmount = response.data.data.totalAmount;
-          console.log('amount', response.data.data);
-        }, this.handleError);
+      if(process.env.NODE_ENV === 'development'){
+        this.operationAmount = json.totalAmount;
+      } else {
+        this.$http.get(this.apiUrl)
+          .then(response => {
+            this.operationAmount = response.data.data.totalAmount;
+            console.log('amount', response.data.data);
+          }, this.handleError);
+      }
     },
     getDate(val) {
       this.date = val;
@@ -73,11 +81,9 @@ export default {
     }
   },
   mounted: function() {
-    if(process.env.NODE_ENV !== 'development'){
-      this.apiUrl = this.$store.getters.reportApiUrl + '/report/check-service/amount?userId=' + this.$store.getters.userId;
-    } else {
-      this.apiUrl = './reportDemoData/reportServiceProvider.json';
-    }
+    this.showEnvVariable();
+
+
   }
 };
 </script>
